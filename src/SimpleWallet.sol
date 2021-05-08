3b4dbb08f35f3fddf983d42cbd5060f55cbf56cf
bxsck <bosstk05nya@gmail.com>
2021-05-08 17:14:55 +0700

    add withdrawable permission

--- src/SimpleWallet.sol
+++ src/SimpleWallet.sol
@@ -2,11 +2,18 @@ pragma solidity ^0.5.13;
 
 contract SimpleWallet {
     
+    address public owner;
+    
+    constructor() public{
+        owner = msg.sender;
+    }
+    
     function () external payable{
         
     }
     
-    function withdrawMoney(address payble _to, uint _amout) public{
+    function withdrawMoney(address payable _to, uint _amount) public{
+        require(owner == msg.sender, "You are not allowed");
         _to.transfer(_amount);
     }
 }
