cf94b0611342121df60f9a2631180c1715d03e0e
bxsck <bosstk05nya@gmail.com>
2021-05-09 02:29:52 +0700

    add event for allowance, moneySent and moneyReceived

--- src/SimpleWallet.sol
+++ src/SimpleWallet.sol
@@ -14,9 +14,11 @@ contract Ownable{
     }
 }
 contract Allowance is Ownable{
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
     mapping(address => uint) public allowance;
     
     function addAllowance(address _who,uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
     
@@ -26,19 +28,25 @@ contract Allowance is Ownable{
     }
    
    function reduceAllowance(address _who, uint _amount) internal{
+       emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who]-_amount);
        allowance[_who] -= _amount;
    }
 }
 contract SimpleWallet is Allowance{
+    
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
         require(_amount <= address(this).balance,"There are not enough funds stored in the smart contracts");
         if(owner!=msg.sender){
             reduceAllowance(msg.sender, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
     
      function () external payable{
+         emit MoneyReceived(msg.sender, msg.value);
         
     }
     
