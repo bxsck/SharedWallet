ab47eb0246247ca5e32d8c5f6c1f671eec24e7a8
bxsck <bosstk05nya@gmail.com>
2021-05-08 17:58:02 +0700

    add allowance func

--- src/SimpleWallet.sol
+++ src/SimpleWallet.sol
@@ -1,15 +1,35 @@
 pragma solidity ^0.5.13;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+//import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
+contract SimpleWallet{
+    address public owner;
     
-    function () external payable{
-        
+    constructor() public{
+        owner = msg.sender;
+    }
+    modifier onlyOwner(){
+        require(owner == msg.sender,"You are not allowed.");
+        _;
     }
     
+    mapping(address => uint) public allowance;
+    
+    function addAllowance(address _who,uint _amount) public onlyOwner{
+        allowance[_who] = _amount;
+    }
+    
+    modifier ownerOrAllowed(uint _amount){
+        require(owner==msg.sender || allowance[msg.sender] >= _amount, "You are not allowed");
+        _;
+    }
+   
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner{
-        
         _to.transfer(_amount);
     }
+    
+     function () external payable{
+        
+    }
+    
 }
